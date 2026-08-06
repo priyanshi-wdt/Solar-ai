82a1ec091ea196f01a918861017e0bac6d98c6a7
priyanshi-wdt <priyanshim.wdt@gmail.com>
2026-08-06 19:24:40 +0530

    change design

--- widget/widget.js
+++ widget/widget.js
@@ -1,8 +1,8 @@
 window.SolarAI = {
   init(config = {}) {
     const iframe = document.createElement("iframe");
-    // iframe.src = `https://solar-ai-livid.vercel.app/?companyId=${encodeURIComponent(config.companyId)}&imageSrc=${encodeURIComponent(config.imageSrc)}`;
-    iframe.src = `http://localhost:5173/?companyId=${encodeURIComponent(config.companyId)}&imageSrc=${encodeURIComponent(config.imageSrc)}`;
+    iframe.src = `https://solar-ai-livid.vercel.app/?companyId=${encodeURIComponent(config.companyId)}&imageSrc=${encodeURIComponent(config.imageSrc)}`;
+    // iframe.src = `http://localhost:5173/?companyId=${encodeURIComponent(config.companyId)}&imageSrc=${encodeURIComponent(config.imageSrc)}`;
     iframe.allow = "microphone";
     iframe.style.position = "absolute";
     iframe.style.bottom = "20px";
